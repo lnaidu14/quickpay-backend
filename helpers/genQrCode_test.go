cfa9abcf9f071e42345d40175d160571875209c1
lalit <lalit.pratap.naidu@gmail.com>
2024-06-08 18:50:46 -0400

    modified setup and teardown

--- helpers/genQrCode_test.go
+++ helpers/genQrCode_test.go
@@ -13,10 +13,11 @@ var unsupportedImgName = "../test/mock/img/unsupportedImg.jpg"
 // TODO: Files not deleting probably due to the test case finishing before the file can be deleted
 // func teardown(file string) {
 // 	os.Remove(file)
-
 // }
 
 func TestString_QrCodeToBase64(t *testing.T) {
+	// setup(unsupportedImgName)
+
 	t.Run("Should return an error if file path doesn't exist", func(t *testing.T) {
 		got := QrCodeToBase64("invalidFilePath")
 		expected := "open invalidFilePath: The system cannot find the file specified."
@@ -27,13 +28,13 @@ func TestString_QrCodeToBase64(t *testing.T) {
 	})
 
 	t.Run("Should return an error if the type of image is not supported", func(t *testing.T) {
-		// setup(unsupportedImgName)
 		got := QrCodeToBase64(unsupportedImgName)
 		expected := "Image type not supported"
 
 		if got != expected {
 			t.Errorf("got %q expected %q", got, expected)
 		}
-		// teardown(unsupportedImgName)
 	})
+
+	// teardown(unsupportedImgName)
 }
